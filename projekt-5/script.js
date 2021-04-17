c0049e0420c9be10944854781f14bc7baf0897c0
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-04-17 22:13:53 +0200

    updated function updateTaskList

--- projekt-5/script.js
+++ projekt-5/script.js
@@ -2,21 +2,20 @@
 var firebaseConfig = {
   apiKey: "AIzaSyAuWQvzIPe1o2rf9wtfb8I0HuMfdve5qf4",
   authDomain: "time-manager-a020f.firebaseapp.com",
-  databaseURL: "https://time-manager-a020f-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://time-manager-a020f-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "time-manager-a020f",
   storageBucket: "time-manager-a020f.appspot.com",
   messagingSenderId: "1091022402112",
-  appId: "1:1091022402112:web:30322b448c92dd12fde5a5"
+  appId: "1:1091022402112:web:30322b448c92dd12fde5a5",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// Add buttons and create function for Toggle button 
+// Add buttons and create function for Toggle button
 
-
-
-let playButton = document.querySelector('#playButton');
-let pauseButton = document.querySelector('#pauseButton');
-let resetButton = document.querySelector('#resetButton');
+let playButton = document.querySelector("#playButton");
+let pauseButton = document.querySelector("#pauseButton");
+let resetButton = document.querySelector("#resetButton");
 
 function toggleButton(buttonKey) {
   const buttonVisible = buttonKey === "PLAY" ? playButton : pauseButton;
@@ -24,8 +23,7 @@ function toggleButton(buttonKey) {
   buttonVisible.style.display = "block";
   buttonHidden.style.display = "none";
   //nie wiem czemu nie działają tutaj ostatnie dwie linijki
-};
-
+}
 
 // Convert time to a format of hours, minutes, seconds.
 
@@ -39,15 +37,12 @@ function timeToString(time) {
   const timeInSec = (timeInMin - mm) * 60;
   const ss = Math.floor(timeInSec);
 
-
   const formattedHH = hh.toString().padStart(2, "0");
   const formattedMM = mm.toString().padStart(2, "0");
   const formattedSS = ss.toString().padStart(2, "0");
 
-
   return `${formattedMM}:${formattedSS}`;
 }
-
 
 // Modify watch textContent
 
@@ -55,15 +50,14 @@ function timeToString(time) {
 // console.log(timeToString(0));
 
 function displayTime(txt) {
-  document.querySelector('#display').textContent = txt;
-};
+  document.querySelector("#display").textContent = txt;
+}
 
 // Create function to start, stop and reset watch
 
 let startTime;
 let elapsedTime = 0;
 let timerInterval;
-
 
 function start() {
   startTime = Date.now() - elapsedTime;
@@ -72,17 +66,16 @@ function start() {
     displayTime(timeToString(elapsedTime));
   }, 1000);
   toggleButton("PAUSE");
-  pauseButton.style.display = "block"
+  pauseButton.style.display = "block";
   playButton.style.display = "none";
-};
-
+}
 
 function pause() {
   clearInterval(timerInterval);
   toggleButton("PLAY");
-  pauseButton.style.display = "none"
+  pauseButton.style.display = "none";
   playButton.style.display = "block";
-};
+}
 
 function reset() {
   clearInterval(timerInterval);
@@ -90,51 +83,49 @@ function reset() {
   elapsedTime = 0;
   toggleButton("PLAY");
   playButton.style.display = "block";
-  pauseButton.style.display = "none"
-};
+  pauseButton.style.display = "none";
+}
 
-
-playButton.addEventListener('click', start);
+playButton.addEventListener("click", start);
 pauseButton.addEventListener("click", pause);
 resetButton.addEventListener("click", reset);
 
 // STWORZYĆ FUNKCJĘ KTÓRA PO 25 MINUTACH ZAPROPONUJE PAUZE I MUZYKE NP. RELAKSACYJNĄ
 //ALBO MUZYKĘ NA ROZŁADOWANIE NAPIĘCIA
 
-
 //////////////////////////////
 // TO DO LIST
 //////////////////////////////
 
-const addToDoButton = document.querySelector('.add-todo');
-const todoContainer = document.querySelector('.todoList');
-const inputField = document.querySelector('#yourtask');
-const addForm = document.querySelector('.form');
+const addToDoButton = document.querySelector(".add-todo");
+const todoContainer = document.querySelector(".todoList");
+const inputField = document.querySelector("#yourtask");
+const addForm = document.querySelector(".form");
 
-//CREATE LABEL FOCUS EVENT 
+//CREATE LABEL FOCUS EVENT
 
-const label = document.querySelector('label');
+const label = document.querySelector("label");
 const userEnteredValue = inputField.value;
 
 const labelUpDownEvent = (value) => {
   const labelUpEvent = document.querySelector('input[type="text"]');
 
-  labelUpEvent.addEventListener('focus', (e) => {
-    label.style.fontSize = '20px';
-    label.style.transform = 'translateY(0px)';
-    label.style.lineHeight = '26px';
+  labelUpEvent.addEventListener("focus", (e) => {
+    label.style.fontSize = "20px";
+    label.style.transform = "translateY(0px)";
+    label.style.lineHeight = "26px";
   });
   const labelDownEvent = document.querySelector('input[type="text"]');
 
-  labelDownEvent.addEventListener('blur', (e) => {
+  labelDownEvent.addEventListener("blur", (e) => {
     if (e.target.value != 0) {
-      label.style.fontSize = '20px';
-      label.style.transform = 'translateY(0px)';
-      label.style.lineHeight = '26px';
+      label.style.fontSize = "20px";
+      label.style.transform = "translateY(0px)";
+      label.style.lineHeight = "26px";
     } else {
-      label.style.fontSize = '26px';
-      label.style.transform = 'translateY(33px)';
-      label.style.lineHeight = '26px';
+      label.style.fontSize = "26px";
+      label.style.transform = "translateY(33px)";
+      label.style.lineHeight = "26px";
     }
   });
 };
@@ -143,22 +134,20 @@ const labelUpDownEvent = (value) => {
 labelUpDownEvent(userEnteredValue);
 //--------
 
-// CREATE TODO EVENT LISTENER FOR INPUT 
-inputField.addEventListener('keydown', (e) => {
+// CREATE TODO EVENT LISTENER FOR INPUT
+inputField.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
-    document.querySelector('.add-todo').click();
+    document.querySelector(".add-todo").click();
   }
 });
-
-
 
 // const template = (todoItem) => `<div class="todoList__user-input userInputBox" data-task-id="${todoItem.id}">
 //               <div class="todoList__buttons">
 //                   <a href="#" class="delete" ><i class="far fa-trash-alt fa-2x"></i>
 //                   <a href="#" class="start "><i class="fa fa-play fa-2x"></i>
 //                   <a href="#" class="stop"><i class="fa fa-plus fa-2x"></i></a>
-//                    </div>   
+//                    </div>
 //                    <p class="paragraph__styling">${todoItem.task}</p>
 //                    </div>`;
 
@@ -169,7 +158,9 @@ inputField.addEventListener('keydown', (e) => {
 // !!!!!!!!!!
 // JAK ZROBIĆ ABY ZADANIA NIE ZNIKAŁY PO ODŚWIEZENIU STRONY
 
-const template = (key) => `<div class="todoList__user-input userInputBox" data-task-id="${key.id}">
+const template = (
+  key
+) => `<div class="todoList__user-input userInputBox" data-task-id="${key.id}">
               <div class="todoList__buttons">
                   <a href="#" class="delete" ><i class="far fa-trash-alt fa-2x"></i>
                   <a href="#" class="start "><i class="far fa-play-circle fa-2x"></i>
@@ -179,41 +170,38 @@ const template = (key) => `<div class="todoList__user-input userInputBox" data-t
                    </div>`;
 
 const updateTaskList = () => {
-  todoContainer.innerHTML = todoItemsArray.map(template).join('')
+  todoContainer.innerHTML = todoItemsArray.map(template).join("");
+  if(todoItemsArray.length == 0){
+    fromInputToDo.textContent = "Nothing to do :)";
+  }
 };
-
-
-
 
 // CREATE TODO EVENT LISTENER FOR ADD BUTTON
 
-addToDoButton.addEventListener('click', function (e) {
+addToDoButton.addEventListener("click", function (e) {
   e.preventDefault();
   const userEnteredValue = inputField.value;
   if (userEnteredValue.trim() != 0) {
-    const key = firebase.database().ref().child('user_task').push().key;
+    const key = firebase.database().ref().child("user_task").push().key;
     console.log(key);
-    const dataTask = { 
+    const dataTask = {
       task: userEnteredValue,
-      time: 'function sum time',
+      time: "function sum time",
       id: Date.now(),
       key: key,
     };
 
-    const updates = {}
-      updates["/user_task/" + key] = dataTask;
-      firebase.database().ref().update(updates)
+    const updates = {};
+    updates["/user_task/" + key] = dataTask;
+    firebase.database().ref().update(updates);
 
-    addTodoItems(userEnteredValue) //function push new items into array
-    inputField.value = '';  // jak jeszcze zresetować input?
+    addTodoItems(userEnteredValue); //function push new items into array
+    inputField.value = ""; // jak jeszcze zresetować input?
     inputField.blur();
-
   } else {
-    alert('Wyznacz sobie cel')
+    alert("Wyznacz sobie cel");
   }
-
 });
-
 
 // LOCAL STORAGE - ARRAY DO PRZECHOWYWANIA NAZWY ZADANIA I FUNKCJI, KTÓRA BY POKAZYWAŁA ŁĄCZNY CZAS PRACY NAD ZADANIEM
 // create function addTodoItems which create todoItems object based on the userEnteredValue, and push it into array
@@ -224,42 +212,39 @@ function addTodoItems(userEnteredValue) {
   const todoItems = {
     task: userEnteredValue,
     id: Date.now(), //co by monzna zrobić z tą datą, a moze niepotrzebna
-    time: 'function sum time', //to jeszcze do wymyślenia 
+    time: "function sum time", //to jeszcze do wymyślenia
   };
   todoItemsArray.push(todoItems);
-  updateTaskList()
-};
+  updateTaskList();
+}
 
 // function addTodoItems(userEnteredValue) {
 //  firebase.database().ref('user_task').once('value', function(snapshot){
 //    snapshot.forEach(function(childSnapshot){
 //      const childKey = childSnapschot.key;
-//      todoItemsArray.push(Object.values(childKey)); 
-     
+//      todoItemsArray.push(Object.values(childKey));
+
 //    });
 //  })
 //   // todoItemsArray.push(todoItems);
 //   updateTaskList()
 // };
 
-
-
-
 /////////////////////
 // FUNKCJA KTÓRA WYŚWIETLA AKTUALNE ZADANIE, DO KTÓREGO BĘDZIE MIERZON CZAS
 ///////////////////
-const input = document.querySelector('input');
-const fromInputToDo = document.querySelector('.current-to-do')
+const input = document.querySelector("input");
+const fromInputToDo = document.querySelector(".current-to-do");
 
 function displayCurrentToDo() {
-  input.addEventListener('input', currentToDo);
+  input.addEventListener("input", currentToDo);
 
   function currentToDo(e) {
     // console.log(e);
     fromInputToDo.textContent = e.target.value;
     e.preventDefault();
   }
-};
+}
 //WYWOŁANIE FUNKCJI DO DODANIA GDY USER WCIŚNIE PLAY
 displayCurrentToDo();
 
@@ -268,7 +253,6 @@ displayCurrentToDo();
 //CZY TU MA BYĆ JAKAŚ PĘTLA ABY DZIAŁAŁO DO KAZDEGO TASK Z OSOBNA
 //CZY POTRZEBNY ZNAK $ PRZY KLASIE PRZYCISKU
 
-
 //!!!!!!!! dlaczego nie działa
 // function removeTaksById(arrOriginal, elementToRemove){
 //   return arrOriginal.filter((item)=>{
@@ -276,88 +260,69 @@ displayCurrentToDo();
 //   });
 // }
 
-  todoContainer.addEventListener('click', (e) => {
-    console.log(e.target);
-    if (e.target.classList.contains('fa-trash-alt')) {
-      console.log(e.target.closest('.userInputBox').dataset.taskId)
+todoContainer.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains("fa-trash-alt")) {
+    console.log(e.target.closest(".userInputBox").dataset.taskId);
 
-      const itemIdString = e.target.closest('.userInputBox').dataset.taskId;
-      console.log(itemIdString);
-      console.log(typeof itemIdString);
+    const itemIdString = e.target.closest(".userInputBox").dataset.taskId;
+    console.log(itemIdString);
+    console.log(typeof itemIdString);
 
-      //zamieniam typ string na int aby móc porównać id 
-     const itemId = parseInt(itemIdString);
-      console.log(typeof itemId);
+    //zamieniam typ string na int aby móc porównać id
+    const itemId = parseInt(itemIdString);
+    console.log(typeof itemId);
 
-      // removeTaksById(todoItemsArray, itemId);
+    // removeTaksById(todoItemsArray, itemId);
 
-
-      //porównuję id w istniejącej tablicy i filtruję,  aby nie zawierała id usuniętego przez urzytkownika
-    const removeItem = todoItemsArray.filter((item)=>{
+    //porównuję id w istniejącej tablicy i filtruję,  aby nie zawierała id usuniętego przez urzytkownika
+    const removeItem = todoItemsArray.filter((item) => {
       return item.id != itemId;
     });
     console.log(todoItemsArray);
     console.log(removeItem);
 
-todoItemsArray = removeItem; // Czy mona to jakoś inaczej zrobić?
+    todoItemsArray = removeItem; // Czy mona to jakoś inaczej zrobić?
 
-updateTaskList();
-fromInputToDo.textContent = 'Nothing to do :)';
-
-// fetch("https://time-manager-a020f-default-rtdb.europe-west1.firebasedatabase.app.json", {
-//   method: 'POST',
-//     body: JSON.stringify([{
-//       id: Date.now(), 
-//       task: userEnteredValue,
-//       time: 'function sum time',}])
-// })
-
-
+    updateTaskList();
   }
 
-if(e.target.classList.contains('fa-play-circle')){
-  startTodo();
-  getElapsedTime();
-}
-
-
-if(e.target.classList.contains('fa-pause-circle')){
-  pauseTodo(); 
-  
-  addTimeToArray();
-  console.log(timeArray);
+  if (e.target.classList.contains("fa-play-circle")) {
+    startTodo();
+    getElapsedTime();
   }
 
- });
+  if (e.target.classList.contains("fa-pause-circle")) {
+    pauseTodo();
 
-let todoTimerInterval;
+    addTimeToArray();
+    console.log(timeArray);
+  }
+});
 
-const getElapsedTime = (time) =>{
-   startTime = Date.now() - elapsedTime;
-    elapsedTime = Date.now() - startTime;
-    displayTime(timeToString(elapsedTime));
- 
+const getElapsedTime = (time) => {
+  startTime = Date.now() - elapsedTime;
+  elapsedTime = Date.now() - startTime;
+  displayTime(timeToString(elapsedTime));
+
   return timeToString(elapsedTime);
 };
 
 const timeArray = [];
 
-function addTimeToArray(elapsedTime){
+function addTimeToArray(elapsedTime) {
   const taskTime = {
     elapsedTime: getElapsedTime(elapsedTime),
-    }
+  };
   timeArray.push(taskTime);
-};
-
-
-
+}
 
 //-----------------
 
-// Add buttons and create function for Toggle button 
+// Add buttons and create function for Toggle button
 
- const playTodoTime = document.querySelector('.fa-play');
- const stopTodoTime = document.querySelector('.fa-plus');
+const playTodoTime = document.querySelector(".fa-play");
+const stopTodoTime = document.querySelector(".fa-plus");
 
 function toggleTodoButton(buttonKey) {
   const buttonVisible = buttonKey === "PLAY" ? playTodoTime : stopTodoTime;
@@ -366,24 +331,21 @@ function toggleTodoButton(buttonKey) {
   // buttonHidden.style.display = "none";
 }
 
-  function startTodo() {
-    startTime = Date.now() - elapsedTime;
-    timerInterval = setInterval(function printTime() {
-      elapsedTime = Date.now() - startTime;
-      displayTime(timeToString(elapsedTime));
-    }, 1000);
-    toggleTodoButton("PAUSE");
+function startTodo() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    displayTime(timeToString(elapsedTime));
+  }, 1000);
+  toggleTodoButton("PAUSE");
 
-    // stopTodoTime.style.display = "block"
-    // playTodoTime.style.display = "none";
-  };
-  
+  // stopTodoTime.style.display = "block"
+  // playTodoTime.style.display = "none";
+}
 
-  
-  function pauseTodo() {
-    clearInterval(timerInterval);
-    toggleTodoButton("PLAY");
-    // pauseButton.style.display = "none"
-    // playButton.style.display = "block";
-  };
-
+function pauseTodo() {
+  clearInterval(timerInterval);
+  toggleTodoButton("PLAY");
+  // pauseButton.style.display = "none"
+  // playButton.style.display = "block";
+}
